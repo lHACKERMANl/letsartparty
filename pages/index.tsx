8115f1e99e3f00a887c516cd484eaadabcf90940
Anisin Vlad <avi@int.spb.ru>
2023-01-10 16:40:40 +0900

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Let's art party</title>
+        <meta name="description" content="Let's art party website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <p className={styles.description}>hello</p>
+      </main>
+    </>
+  )
+}
