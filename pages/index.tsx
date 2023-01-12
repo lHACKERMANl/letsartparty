800b97744e2fc978eece07bb495ad5b77e8e67bb
Anisin Vlad <avi@int.spb.ru>
2023-01-13 00:22:58 +0900

    push to dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import HeaderBar from '../components/HeaderBar'
 
 export default function Home() {
   return (
@@ -11,11 +9,14 @@ export default function Home() {
       <Head>
         <title>Let's art party</title>
         <meta name="description" content="Let's art party website" />
-        <meta name="viewport" content="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="chat-notification-content">
-        <p className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl">hello</p>
+        <header>
+          <HeaderBar></HeaderBar>
+
+        </header>
       </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        'Italiana': ["Italiana", "serif"],
+        'Raleway': ["Raleway", "sans-serif"]
+      },
+    },
+  }, 
   plugins: [],
 }
