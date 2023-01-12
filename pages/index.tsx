5b7ad74b2df5fea05b9ab2437b2a7dd2d4e5b766
Anisin Vlad <avi@int.spb.ru>
2023-01-13 00:27:43 +0900

    clean imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import HeaderBar from '../components/HeaderBar'
 
 export default function Home() {
